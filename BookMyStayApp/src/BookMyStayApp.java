991d6a82708cf2bb6f5dc551f1a28190f5eb09f6
Likesh1235 <sonysevvanam@gmail.com>
2026-03-24 14:22:15 +0530

    UC1-Welcome Message

---
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -0,0 +1,10 @@
+public class BookMyStayApp {
+    public static void main(String[] args)
+    {
+        System.out.println("Hotel Booking System v1.0\n");
+        System.out.println("Welcome to the Hotel Booking Management System\n");
+        System.out.println("System initialized Successfully");
+
+    }
+
+}
